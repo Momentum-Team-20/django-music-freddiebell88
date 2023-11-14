9dc075715f8a1a058a03d04508f75199e017bdeb
freddiebell88 <frederikcwbell@gmail.com>
2023-11-14 11:37:33 -0500

    css not connecting

--- albums/models.py
+++ albums/models.py
@@ -4,7 +4,14 @@ from django.db import models
 class Album(models.Model):
     title = models.CharField(max_length=250)
     artist_name = models.CharField(max_length=250)
-    created_at = models.DateField(null=True, blank=True)
+    created_at = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.title
+
+
+# class Collection(models.Model):
+#     name = models.CharField(max_length=250)
+
+#     def __str__(self):
+#         return self.name
--- albums/urls.py
+++ albums/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import list_albums
+# from .views import list_albums
 
 urlpatterns = [
     path('', views.list_albums, name='home'),
